40b2b299e11961ecb94c7688197b74638a2c7ab3
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-15 20:00:41 -0300

    feat(provider): adds an unique app provider

---
+++ src/contexts/provider.tsx
@@ -0,0 +1,28 @@
+import type { ReactNode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { AuthenticationProvider } from './authentication'
+import { LanguageProvider } from './language'
+import { ThemeProvider } from './theme'
+
+type Props = {
+  children: ReactNode
+}
+
+const queryClient = new QueryClient()
+
+export function Provider(props: Props) {
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <QueryClientProvider client={queryClient}>
+          <AuthenticationProvider>
+            <BrowserRouter {...props} />
+          </AuthenticationProvider>
+        </QueryClientProvider>
+      </LanguageProvider>
+    </ThemeProvider>
+  )
+}

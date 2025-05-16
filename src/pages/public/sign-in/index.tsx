d9b26876ee34e59ff64790c8dec4a38bc495fbc7
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-16 10:45:28 -0300

    feat(sign-in): improves page

--- src/pages/public/sign-in/index.tsx
+++ src/pages/public/sign-in/index.tsx
@@ -1,3 +1,53 @@
+import { useTranslation } from 'react-i18next'
+import { Sparkles } from 'lucide-react'
+
+import { SEO } from '@/components/seo'
+import {
+  BasicPageBody,
+  BasicPageDescription,
+  BasicPageFooter,
+  BasicPageHeader,
+  BasicPageLogoContainer,
+  BasicPageRoot,
+  BasicPageTitle,
+  BasicPageWrapper,
+} from '@/components/ui/basic-page'
+
 export default function SignInPage() {
-  return <h1>Página de login</h1>
+  const { t } = useTranslation('sign-in')
+
+  return (
+    <BasicPageWrapper>
+      <BasicPageRoot>
+        <SEO title={t('meta.title')} />
+
+        <BasicPageLogoContainer>
+          <Sparkles />
+        </BasicPageLogoContainer>
+
+        <BasicPageHeader>
+          <BasicPageTitle>{t('page.title')}</BasicPageTitle>
+          <BasicPageDescription>{t('page.description')}</BasicPageDescription>
+        </BasicPageHeader>
+
+        <BasicPageBody>
+          {/* <SignInForm /> */}
+        </BasicPageBody>
+
+        <BasicPageFooter>
+          {/* <BasicPageText>
+            <BasicPageLink href="/forgot-password">
+              {t('forgot-password.link.label')}
+            </BasicPageLink>
+          </BasicPageText> */}
+
+          {/* <BasicPageText>
+            {t('page.create-account.text')} <BasicPageLink href="/sign-up">
+              {t('page.create-account.link.label')}
+            </BasicPageLink>
+          </BasicPageText> */}
+        </BasicPageFooter>
+      </BasicPageRoot>
+    </BasicPageWrapper>
+  )
 }

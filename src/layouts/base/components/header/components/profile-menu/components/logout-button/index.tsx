91e302b17aad14c8f65bd1411491577e8feb2db5
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-16 17:39:14 -0300

    fix(profile-menu): sign out button imports (#24)

--- src/layouts/base/components/header/components/profile-menu/components/logout-button/index.tsx
+++ src/layouts/base/components/header/components/profile-menu/components/logout-button/index.tsx
@@ -2,20 +2,30 @@ import { useCallback } from "react"
 import { useTranslation } from "react-i18next"
 import { LogOut } from "lucide-react"
 
+import { useConfirmation } from "@/hooks/user-confirmation"
+import { useAuthentication } from "@/hooks/use-authentication"
+
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
 
 export function LogoutButton() {
   const { t } = useTranslation('base-layout', { keyPrefix: 'header.profile-menu.sign-out' })
+  const { signOut } = useAuthentication()
+  const { showConfirmation } = useConfirmation()
 
   const handleConfirm = useCallback(
-    () => {
-      // TODO: Confirmar antes de fazer logout
-    },
-    [],
+    () => showConfirmation({
+      icon: LogOut,
+      title: t('confirmation-modal.title'),
+      description: t('confirmation-modal.description'),
+      cancelButton: { text: t('confirmation-modal.cancel-button.label') },
+      confirmButton: { text: t('confirmation-modal.confirm-button.label') },
+      onConfirm: signOut,
+    }),
+    [showConfirmation, signOut, t],
   )
 
   return (
-    <DropdownMenuItem variant="destructive" onClick={handleConfirm} disabled>
+    <DropdownMenuItem variant="destructive" onClick={handleConfirm}>
       <LogOut className="size-4" />
       {t('label')}
     </DropdownMenuItem>

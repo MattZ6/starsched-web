9b36111dfb8ec989654eacb5032a27757e57fbcd
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-19 11:09:18 -0300

    feat: loading indicator (#27)
    * feat(ui): adds loading indicator component
    * chore(account-confirmation-loading): uses loading indicator component

---
+++ src/components/ui/loading-indicator.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils"
+
+type Props = React.ComponentProps<"svg">
+
+export function LoadingIndicator({ className, ...props }: Props) {
+  return (
+    <svg
+      viewBox="0 0 50 50"
+      className={cn("size-6 animate-spinner-rotate", className)}
+      {...props}
+    >
+      <circle
+        cx="25"
+        cy="25"
+        r="20"
+        fill="none"
+        strokeWidth="5"
+        stroke-linecap="round"
+        className="stroke-primary animate-spinner-dash"
+      />
+    </svg>
+  )
+}

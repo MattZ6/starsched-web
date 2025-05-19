e17fe1849a75b7a1ae93c312be354011c162349e
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-19 15:28:43 -0300

    refactor: get my profile (#35)
    * refactor(get-my-profile): adapt service to throw on error
    * chore(use-get-my-profile): add stale time and remove refretch config
    * refactor(profile-menu): adapt to hook new implementation

--- src/hooks/services/starsched/use-get-my-profile.ts
+++ src/hooks/services/starsched/use-get-my-profile.ts
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query"
 export function useGetMyProfile() {
   return useQuery({
     queryKey: ['my-profile'],
-    refetchOnWindowFocus: false,
+    staleTime: 1 * 24 * 60 * 60 * 1000, // 👈 1 dia
     queryFn: ({ signal }) => getMyProfile({ abortSignal: signal })
   })
 }

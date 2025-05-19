d035c7739ec3e5363c71c07c3e1c802a41ae0811
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-19 10:23:17 -0300

    chore(vite-config): add open to ser config on dev mode (#25)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  server: {
+    open: true,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {

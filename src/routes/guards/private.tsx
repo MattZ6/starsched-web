4a3e1f677dc97672cec9d5fa5bc8547dbd8a8cc7
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-20 09:32:48 -0300

    feat(private-routes-nav-guard): adds profile check step

--- src/routes/guards/private.tsx
+++ src/routes/guards/private.tsx
@@ -1,6 +1,58 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
 import { useAuthentication } from '@/hooks/use-authentication'
+import { useGetMyProfile } from '@/hooks/services/starsched/use-get-my-profile'
+
+import { LoadingIndicator } from '@/components/ui/loading-indicator'
+import {
+  BasicPageBody,
+  BasicPageDescription,
+  BasicPageHeader,
+  BasicPageLogoContainer,
+  BasicPageRoot,
+  BasicPageTitle,
+  BasicPageWrapper,
+} from '@/components/ui/basic-page'
+
+function ProfileCheck() {
+  const { isLoading, error } = useGetMyProfile()
+
+  if (isLoading) {
+    return <BasicPageWrapper>
+      <BasicPageRoot>
+        <BasicPageBody >
+          <BasicPageLogoContainer>
+            <LoadingIndicator />
+          </BasicPageLogoContainer>
+
+          <BasicPageHeader>
+            <BasicPageTitle>Carregando perfil...</BasicPageTitle>
+            <BasicPageDescription>Aguarde alguns instantes</BasicPageDescription>
+          </BasicPageHeader>
+        </BasicPageBody>
+      </BasicPageRoot>
+    </BasicPageWrapper>
+  }
+
+  if (error) {
+    return <BasicPageWrapper>
+      <BasicPageRoot>
+        <BasicPageBody >
+          <BasicPageLogoContainer>
+
+          </BasicPageLogoContainer>
+
+          <BasicPageHeader>
+            <BasicPageTitle>Falha</BasicPageTitle>
+            <BasicPageDescription>Falha ao carregar o seu perfil</BasicPageDescription>
+          </BasicPageHeader>
+        </BasicPageBody>
+      </BasicPageRoot>
+    </BasicPageWrapper>
+  }
+
+  return <Outlet />
+}
 
 export function PrivateRoutesNavGuard() {
   const { user } = useAuthentication()
@@ -14,5 +66,5 @@ export function PrivateRoutesNavGuard() {
     return <Navigate to="/sign-in" state={{ from: location }} />
   }
 
-  return <Outlet />
+  return <ProfileCheck />
 }

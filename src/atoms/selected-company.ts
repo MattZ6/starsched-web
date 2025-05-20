76769c3ef29ba5880f9d4eecdc3aec684caf93c9
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-20 09:24:36 -0300

    chore(selected-company-slug-atom): rename and persist state (#41)

--- src/atoms/selected-company.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "jotai";
-
-import type { Company } from "@starsched/sdk";
-
-type SelectedCompanyState = Company | null
-
-export const selectedCompanyAtom = atom<SelectedCompanyState>(null)

4fa2541cc91a0d61a9c39f2a0cfd3dace2563af2
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-29 16:57:13 -0300

    feat(create-company-invite): refetch all pages on delete

--- src/hooks/services/starsched/use-create-company-invite.ts
+++ src/hooks/services/starsched/use-create-company-invite.ts
@@ -2,13 +2,22 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import { createCompanyInvite } from "@/services/starsched/create-company-invite"
 
+import { EventUtils } from "@/utils/event"
+
+const eventUtils = new EventUtils()
+const RESET_INVITES_LIST_EVENT_NAME = 'reset-company-invitations-list'
+
 export function useCreateCompanyInvite() {
   const queryClient = useQueryClient()
 
   return useMutation({
     mutationFn: createCompanyInvite,
-    onSuccess: (_, input) => queryClient.refetchQueries({
-      queryKey: ['company-invitations', input.companyId]
-    })
+    onSuccess: (_, input) => {
+      eventUtils.emit(RESET_INVITES_LIST_EVENT_NAME)
+
+      queryClient.refetchQueries({
+        queryKey: ['company-invitations', input.companyId]
+      })
+    }
   })
 }

9ffb91d618bdc288883312fe370ecdd78a8b8abc
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-29 16:33:54 -0300

    feat(update-company-invite-role): refetch only current invites page

--- src/hooks/services/starsched/use-update-company-invite-role.ts
+++ src/hooks/services/starsched/use-update-company-invite-role.ts
@@ -1,14 +1,15 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query"
 
 import { updateCompanyInviteRole } from "@/services/starsched/update-company-invite-role"
 
-export function useUpdateCompanyInviteRole() {
-  const queryClient = useQueryClient()
+import { EventUtils } from "@/utils/event"
 
+const eventUtils = new EventUtils()
+const REFETCH_INVITES_PAGE_EVENT_NAME = 'refetch-company-invitations-page'
+
+export function useUpdateCompanyInviteRole() {
   return useMutation({
     mutationFn: updateCompanyInviteRole,
-    onSuccess: (_, input) => queryClient.refetchQueries({
-      queryKey: ['company-invitations', input.companyId]
-    })
+    onSuccess: () => eventUtils.emit(REFETCH_INVITES_PAGE_EVENT_NAME)
   })
 }
--- src/pages/private/invites/components/page-content/index.tsx
+++ src/pages/private/invites/components/page-content/index.tsx
@@ -1,13 +1,18 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { useGetCompanyInvitations } from "@/hooks/services/starsched/use-get-company-invitations";
 import { useSelectedCompany } from "@/hooks/use-selected-company";
+
+import { EventUtils } from "@/utils/event";
 
 import { Loading } from "./components/loading";
 import { Failure } from "./components/failure";
 import { InvitesTable } from "./components/invites-table";
 
 const ITEMS_PER_PAGE = 5
+
+const eventUtils = new EventUtils()
+const REFETCH_INVITES_PAGE_EVENT_NAME = 'refetch-company-invitations-page'
 
 export function PageContent() {
   const selectedCompany = useSelectedCompany()
@@ -20,8 +25,14 @@ export function PageContent() {
     page
   })
 
-  const handleGoToFirstPage = useCallback(() => setPage(1), [])
-  const handleGoToPreviousPage = useCallback(() => setPage(currentPage => currentPage <= 1 ? 1 : currentPage - 1), [])
+  const handleGoToFirstPage = useCallback(() => {
+    setPage(1)
+  }, [])
+
+  const handleGoToPreviousPage = useCallback(() => {
+    setPage(currentPage => currentPage <= 1 ? 1 : currentPage - 1)
+  }, [])
+
   const handleGoToNextPage = useCallback(() => {
     if (data) {
       const totalPages = Math.ceil(data.total_items_count / ITEMS_PER_PAGE)
@@ -29,11 +40,19 @@ export function PageContent() {
       setPage(currentPage => currentPage >= totalPages ? totalPages : currentPage + 1)
     }
   }, [data])
+
   const handleGoToLastPage = useCallback(() => {
     if (data) {
       setPage(Math.ceil(data.total_items_count / ITEMS_PER_PAGE))
     }
   }, [data])
+
+  useEffect(() => {
+    eventUtils.subscribe(REFETCH_INVITES_PAGE_EVENT_NAME, () => refetch())
+
+    return () => eventUtils.unsubscribe(REFETCH_INVITES_PAGE_EVENT_NAME, () => { })
+
+  }, [refetch])
 
   if (isLoading) {
     return <Loading itemsPerPage={ITEMS_PER_PAGE} />

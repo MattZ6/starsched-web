ff8ce9380bee6f085dbc96b86396e5fc73c3202d
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-05-30 15:31:25 -0300

    feat(company-invites): prevent not allowed users to view invites page (#82)

--- src/layouts/team/components/tabs-nav/index.tsx
+++ src/layouts/team/components/tabs-nav/index.tsx
@@ -1,11 +1,12 @@
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-type Params = {
-  companySlug: string
-}
+import { useSelectedCompany } from "@/hooks/use-selected-company";
 
 export function TabsNav() {
-  const { companySlug } = useParams<Params>()
+  const selectedCompany = useSelectedCompany()
+  const companySlug = selectedCompany!.slug
+
+  const canViewInvites = selectedCompany?.role === 'owner' || selectedCompany?.role === 'manager'
 
   return (
     <div className="inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground w-fit">
@@ -17,13 +18,16 @@ export function TabsNav() {
       >
         Membros
       </NavLink>
-      <NavLink
-        to={`/${companySlug}/team/invites`}
-        end
-        className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-[current=page]:bg-background aria-[current=page]:text-foreground aria-[current=page]:shadow"
-      >
-        Convites
-      </NavLink>
+
+      {canViewInvites && (
+        <NavLink
+          to={`/${companySlug}/team/invites`}
+          end
+          className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-[current=page]:bg-background aria-[current=page]:text-foreground aria-[current=page]:shadow"
+        >
+          Convites
+        </NavLink>
+      )}
     </div>
   )
 }

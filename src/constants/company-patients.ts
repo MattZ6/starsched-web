9ff4e4ea1a80269b0367a9dcb379680866552322
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-06-13 18:28:56 -0300

    feat(company-patients): add page and route

---
+++ src/constants/company-patients.ts
@@ -0,0 +1,6 @@
+export const companyPatientsEventNames = {
+  /** Evento disparado para resetar o cache da lista de pacientes da clínica. */
+  RESET_LIST: 'reset-company-patients-list',
+  /** Evento disparado para resetar o cache página atual da lista de pacientes da clínica. */
+  RESET_PAGE: 'reset-company-patients-page',
+}
--- src/layouts/patients/components/tabs-nav/index.tsx
+++ src/layouts/patients/components/tabs-nav/index.tsx
@@ -22,13 +22,13 @@ export function TabsNav() {
         Todos
       </NavLink>
 
-      <NavLink
+      {/* <NavLink
         to={`/${companySlug}/patients/mine`}
         end
         className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-[current=page]:bg-background aria-[current=page]:text-foreground aria-[current=page]:shadow"
       >
         Meus pacientes
-      </NavLink>
+      </NavLink> */}
     </div>
   )
 }
